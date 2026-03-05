b84e85f2fb70191151c090294149510ff09d639c
João Victor Haas <joao.haas@alunos.bpkedu.com.br>
2026-03-04 21:40:17 -0300

    analisePJ e analisePF

---
+++ src/main/java/br/com/nogueiranogueira/aularefatoracao/strategy/AnalisePF.java
@@ -0,0 +1,22 @@
+package br.com.nogueiranogueira.aularefatoracao.strategy;
+
+import br.com.nogueiranogueira.aularefatoracao.dto.SolicitacaoAnalise;
+import br.com.nogueiranogueira.aularefatoracao.dto.TipoConta;
+
+public class AnalisePF implements SolicitacaoStrategy{
+
+    @Override
+    public boolean Analisar(SolicitacaoAnalise solicitacao) {
+        if (solicitacao.valor() > 50000 && solicitacao.score() < 700) {
+            System.out.println("Reprovado: Risco PJ");
+            return false;
+        }
+        System.out.println("Aprovado PJ");
+        return true;
+    }
+
+    @Override
+    public boolean Elegivel(SolicitacaoAnalise solicitacao) {
+        return solicitacao.tipoConta().equals(TipoConta.PF);
+    }
+}
